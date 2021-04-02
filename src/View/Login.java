c6f5a4a5ddec3294354c561a1382f15964089de8
victor002Hugo <victorsouvilela@hotmail.com>
2021-04-02 15:19:35 -0300

    Acabando a primeira tela

--- src/View/Login.java
+++ src/View/Login.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class Login extends JFrame {
     private JPanel panel1;
+    private JTextField UsuarioCampo;
+    private JButton button1;
+    private JPasswordField SenhaCampo;
+    private JLabel Usuario;
+    private JLabel Senha;
+    private JLabel Login;
 
     public Login(){
         add(panel1);
@@ -11,4 +17,6 @@ public class Login extends JFrame {
         setSize(600,700);
 
     }
+
+
 }
